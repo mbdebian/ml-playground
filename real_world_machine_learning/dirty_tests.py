053e1ec3b5ca03659cd71a490a8e85b1e6c9d8a2
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-09-13 19:29:05 +0100

    script for dirty tests on machine learning concepts

---
+++ real_world_machine_learning/dirty_tests.py
@@ -0,0 +1,12 @@
+# 
+# Author    : Manuel Bernal Llinares
+# Project   : ml-playground
+# Timestamp : 13-09-2017 19:28
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+Document your module here
+"""
