3369047534c9297fbf4099e97491401379e8da59
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-10-19 11:01:51 +0100

    helper functions section

--- real_world_machine_learning/playground/chapter3.py
+++ real_world_machine_learning/playground/chapter3.py
@@ -13,6 +13,9 @@ Scratchpad for the chapter 3 from the book
 
 import pandas
 
+# Helper functions
+
+
 # Read the Titanic sample data
 sample_data_titanic = pandas.read_csv("../book_code/data/titanic.csv")
 print("---> Sample data - Titanic, #{} entries".format(len(sample_data_titanic)))
