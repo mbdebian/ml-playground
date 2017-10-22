e4c7145aaa0e2c5f0cc766c08c8a96d71a729cbc
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-10-22 13:08:47 +0100

    possible localization problem

--- real_world_machine_learning/playground/chapter3.py
+++ real_world_machine_learning/playground/chapter3.py
@@ -56,6 +56,9 @@ def prepare_data(data):
 
 # Read the Titanic sample data
 sample_data_titanic = pandas.read_csv("../book_code/data/titanic.csv")
+# The data is English localized, but pandas will fail trying to convert numbers into float because this system is
+# Spanish localized
+sample_data_titanic['Fare'] = sample_data_titanic['Fare'].replace('.', ',')
 print("---> Sample data - Titanic, #{} entries".format(len(sample_data_titanic)))
 print("... Sample ...\n"
       "{}\n"
