b0024775229921f88bf2b2317679171a762973f8
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-10-26 09:51:10 +0100

    chapter 4 playground

---
+++ real_world_machine_learning/playground/chapter4.py
@@ -0,0 +1,12 @@
+# 
+# Author    : Manuel Bernal Llinares
+# Project   : ml-playground
+# Timestamp : 26-10-2017 9:50
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+Document your module here
+"""
