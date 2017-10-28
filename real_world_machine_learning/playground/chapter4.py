a4e776b48b55d7373dab84559a3423c97919e971
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-10-28 20:45:27 +0100

    target value test shape

--- real_world_machine_learning/playground/chapter4.py
+++ real_world_machine_learning/playground/chapter4.py
@@ -48,4 +48,4 @@ target_test = target[idx_test]
 print("---> Train dataset shape {}".format(features_train.shape))
 print("---> Test dataset shape {}".format(features_test.shape))
 print("---> Target data for the training dataset, shape {}".format(target_train.shape))
-print("---> Target data for the test dataset, shape {}")
+print("---> Target data for the test dataset, shape {}".format(target_test.shape))
