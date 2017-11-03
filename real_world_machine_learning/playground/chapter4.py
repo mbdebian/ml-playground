150ee28a5f992a70ae52c054b0387b6c8aa92567
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-11-03 11:25:56 +0000

    separate the target true values

--- real_world_machine_learning/playground/chapter4.py
+++ real_world_machine_learning/playground/chapter4.py
@@ -184,6 +184,8 @@ print("[{} ============================= {}]\n\n".format("-" * padding, "-" * pa
 print("[{} Grid Search with Kernel-SVM Model {}]".format("-" * padding, "-" * padding))
 print("+++> Load the Titanic dataset")
 titanic_dataset = pandas.read_csv("../book_code/data/titanic.csv")
+y = titanic_dataset['Survived']
+
 print("[{} ================================= {}]\n\n".format("-" * padding, "-" * padding))
 
 # Show all plots
