669312c4ccab65ee04485875da8693be37651771
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-11-07 14:59:39 +0000

    chapter6, first practical

---
+++ real_world_machine_learning/playground/chapter6.py
@@ -0,0 +1,12 @@
+# 
+# Author    : Manuel Bernal Llinares
+# Project   : ml-playground
+# Timestamp : 07-11-2017 14:59
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+Document your module here
+"""
