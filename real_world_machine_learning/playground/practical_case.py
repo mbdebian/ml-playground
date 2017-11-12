95c275bbf5e6ca703eadabebee1ca5dd3370b191
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-11-12 08:58:07 +0000

    placeholder for the practical case from the book

---
+++ real_world_machine_learning/playground/practical_case.py
@@ -0,0 +1,12 @@
+# 
+# Author    : Manuel Bernal Llinares
+# Project   : ml-playground
+# Timestamp : 12-11-2017 8:57
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+Document your module here
+"""
